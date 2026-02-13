7da776972d32eaf31fc6d33651a6a593311cfe5a
avi <contact@emultiskills.com>
2026-02-13 14:46:07 +0530

    Update: 2026-02-13 14:46:07

--- sync.sh
+++ sync.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-cd /c/EMS/Terraform/timescaledb-complete-course || exit
+
+git config core.safecrlf false
+git config core.autocrlf true
+
+echo "Syncing to GitHub..."
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$BRANCH" = "master" ]; then
+    git branch -M main
+    BRANCH="main"
+fi
+
 git add .
-git commit -m "Auto-sync: $(date '+%Y-%m-%d %H:%M:%S')"
-git push origin main --force
+
+read -p "Commit message: " msg
+
+if [ -z "$msg" ]; then
+    msg="Update: $(date '+%Y-%m-%d %H:%M:%S')"
+fi
+
+git commit -m "$msg"
+git push -u origin $BRANCH
+
+echo "Done!"
